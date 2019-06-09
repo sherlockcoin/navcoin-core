c475e21be9e00d79dec9163a2dc37471ce85c36c
sherlockcoin <sherlockcoin@gmail.com>
2019-06-10 05:25:12 +0530

    [OSX] Dependiencies Update
    Dependencies updated.

--- depends/packages/native_biplist.mk
+++ depends/packages/native_biplist.mk
@@ -1,15 +1,15 @@
-package=native_biplist
-$(package)_version=1.0.3
-$(package)_download_path=https://bitbucket.org/wooster/biplist/downloads
-$(package)_file_name=biplist-$($(package)_version).tar.gz
-$(package)_sha256_hash=4c0549764c5fe50b28042ec21aa2e14fe1a2224e239a1dae77d9e7f3932aa4c6
-$(package)_install_libdir=$(build_prefix)/lib/python3/dist-packages
+package=native_mac_alias
+$(package)_version=2.0.7
+$(package)_download_path=https://github.com/al45tair/mac_alias/archive/
+$(package)_file_name=v$($(package)_version).tar.gz
+$(package)_sha256_hash=6f606d3b6bccd2112aeabf1a063f5b5ece87005a5d7e97c8faca23b916e88838
+$(package)_install_libdir=$(build_prefix)/lib/python/dist-packages
 
 define $(package)_build_cmds
-    python3 setup.py build
+    python setup.py build
 endef
 
 define $(package)_stage_cmds
     mkdir -p $($(package)_install_libdir) && \
-    python3 setup.py install --root=$($(package)_staging_dir) --prefix=$(build_prefix) --install-lib=$($(package)_install_libdir)
+    python setup.py install --root=$($(package)_staging_dir) --prefix=$(build_prefix) --install-lib=$($(package)_install_libdir)
 endef
